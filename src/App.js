0254fb878777ab3972a828f10ec91cac4b3e5bcd
Preeti <singhp1992@gmail.com>
2019-06-24 13:08:54 +0200

    importing header into the app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 
 class App extends Component {
   render() {
