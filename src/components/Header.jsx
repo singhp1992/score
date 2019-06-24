a4f80ef8ba56e4e8fe5cb7f746c5e80b54a9162b
Preeti <singhp1992@gmail.com>
2019-06-24 13:08:03 +0200

    created header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,5 +2,10 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 function Header() {
-
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
 }
