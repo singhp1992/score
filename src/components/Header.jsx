0c187804b19e74de631ae85da902d9de14bcd116
Preeti <singhp1992@gmail.com>
2019-06-24 14:16:19 +0200

    passed props thru header comp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import props from "react"
 
 function Header(props) {
+    console.log(props)
     return (
         <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
+            <h1>{props.title}</h1>
+            <span className="stats">Players: {props.totalPlayers }</span>
         </header>
     );
 }
