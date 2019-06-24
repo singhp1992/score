277acd6fdf0ed7b026cee1424f08313e30048763
Preeti <singhp1992@gmail.com>
2019-06-24 13:39:55 +0200

    importing the counter component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
 import Player from './components/Player'
+import Counter from './components/Counter'
 
 class App extends Component {
   render() {
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,12 +7,6 @@ function Player() {
             <span className="player-name">
                 Preeti
             </span>
-
-            <div className="counter">
-                <button className="counter-action decrement"> - </button>
-                <span className="counter-score"> 6 </span>
-                <button className="counter-action increment"> + </button>
-            </div>
         </div>
 
     );
