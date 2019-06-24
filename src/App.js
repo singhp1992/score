409b17b98aef92edb20c0b96561c83f03b9fecc4
Preeti <singhp1992@gmail.com>
2019-06-24 13:37:12 +0200

    imported player component to app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import Player from './components/Player'
 
 class App extends Component {
   render() {
     return (
       <div id="root">
         <Header />
+        <Player />
       </div>
     );
   }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -10,7 +10,7 @@ function Player() {
 
             <div className="counter">
                 <button className="counter-action decrement"> - </button>
-                <span className="counter-score"></span>
+                <span className="counter-score"> 6 </span>
                 <button className="counter-action increment"> + </button>
             </div>
         </div>
