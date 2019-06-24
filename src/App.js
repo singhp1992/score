6e0c6f2bda0e2b75729bdb94b4387f0bfd3d9308
Preeti <singhp1992@gmail.com>
2019-06-24 13:11:01 +0200

    removing react css class from div

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header'
 class App extends Component {
   render() {
     return (
-      <div className="App root">
+      <div className="root">
         <Header />
       </div>
     );
