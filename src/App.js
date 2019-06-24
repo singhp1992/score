ae29a1f69c2fcd70cb83ec812dc67240d38ae8d4
Preeti <singhp1992@gmail.com>
2019-06-24 13:09:44 +0200

    header showing up

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import Header from './components/Header'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App root">
         <div className="App-header">
           <h2>Interactive Scoreboard</h2>
         </div>
+        <Header />
       </div>
     );
   }
