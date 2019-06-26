4d43576144f1ceaadfb7840f0bfc6347ed8c1cfd
Preeti <singhp1992@gmail.com>
2019-06-26 11:00:18 +0200

    removed old class component on app page

--- src/App.js
+++ src/App.js
@@ -3,30 +3,6 @@ import './App.css';
 import Header from './components/Header'
 import Player from './components/Player'
 
-// class App extends Component {
-//   render(props) {
-//     return (
-//       <div className="scoreboard" id="root">
-//         <Header title="Scoreboard" totalPlayers={1} />
-
-//         {/* Player List */}
-//         {props.players.map( player => 
-//           <Player 
-//             name={player.name} 
-//             score={player.score} 
-//           />
-//         )}
-
-
-        
-//         {/* <Player name="Bani" score={8} />
-//         <Player name="Simi" score={7} />
-//         <Player name="Simon" score={5} /> */}
-//       </div>
-//     );
-//   }
-// }
-
 const App = (props) => {
   return(
     <div className="scoreboard" id="root">
