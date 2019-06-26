5492e939924d583ad897100c24a9455a3bbd0567
Preeti <singhp1992@gmail.com>
2019-06-26 10:58:50 +0200

    deleted array from app.js

--- src/App.js
+++ src/App.js
@@ -3,46 +3,44 @@ import './App.css';
 import Header from './components/Header'
 import Player from './components/Player'
 
-const players = [{
-  name: "Guil",
-  score: 50
-},
-{
-  name: "Treasure",
-  score: 85
-},
-{
-  name: "Ashley",
-  score: 95
-},
-{
-  name: "James",
-  score: 80
-}
-]
+// class App extends Component {
+//   render(props) {
+//     return (
+//       <div className="scoreboard" id="root">
+//         <Header title="Scoreboard" totalPlayers={1} />
 
-class App extends Component {
-  render(props) {
-    return (
-      <div className="scoreboard" id="root">
-        <Header title="Scoreboard" totalPlayers={1} />
-
-        {/* Player List */}
-        {props.players.map( player => 
-          <Player 
-            name="Preeti" 
-            score={0} 
-          />
-        )}
+//         {/* Player List */}
+//         {props.players.map( player => 
+//           <Player 
+//             name={player.name} 
+//             score={player.score} 
+//           />
+//         )}
 
 
         
-        <Player name="Bani" score={8} />
-        <Player name="Simi" score={7} />
-        <Player name="Simon" score={5} />
-      </div>
-    );
-  }
+//         {/* <Player name="Bani" score={8} />
+//         <Player name="Simi" score={7} />
+//         <Player name="Simon" score={5} /> */}
+//       </div>
+//     );
+//   }
+// }
+
+const App = (props) => {
+  return(
+    <div className="scoreboard" id="root">
+         <Header title="Scoreboard" totalPlayers={1} />
+
+      {/* Player List */}
+         {props.initialPlayers.map( player => 
+          <Player 
+            name={player.name} 
+            score={player.score} 
+          />
+        )}
+    </div>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,25 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const players = [{
+  name: "Guil",
+  score: 50
+},
+{
+  name: "Treasure",
+  score: 85
+},
+{
+  name: "Ashley",
+  score: 95
+},
+{
+  name: "James",
+  score: 80
+}
+]
+
 ReactDOM.render(
-  <App />,
+  <App initialPlayers={players}/>,
   document.getElementById('root')
 );
