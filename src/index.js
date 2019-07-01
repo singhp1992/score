346a94087d676daf830e227feb66bd83b338302e
Preeti <singhp1992@gmail.com>
2019-07-01 22:38:52 +0200

    removing player object from app

--- src/index.js
+++ src/index.js
@@ -3,28 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-// const players = [{
-//   name: "Preeti",
-//   score: 50,
-//   id: 1
-// },
-// {
-//   name: "Simon",
-//   score: 85,
-//   id: 2
-// },
-// {
-//   name: "Bani",
-//   score: 95,
-//   id: 3
-// },
-// {
-//   name: "Simi",
-//   score: 80,
-//   id: 4
-// }
-// ]
-
 ReactDOM.render(
   <App/>,
   document.getElementById('root')
