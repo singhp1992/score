e2e358c4fde154c94eeda91e6da55a958ed752e0
Preeti <singhp1992@gmail.com>
2019-07-01 22:53:29 +0200

    props working properly!

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,7 +5,7 @@ function Player(props) {
     return (
         <div className="player">
             <span className="player-name">
-                <button className="remove-player" onClick={ () => prop.removePlayer(props.id)}> x </button>
+                <button className="remove-player" onClick={ () => props.removePlayer(props.id)}> x </button>
                 { props.name }
             </span>
 
