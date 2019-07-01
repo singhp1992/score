1d5f2d0610242b708846127460580106edf97883
Preeti <singhp1992@gmail.com>
2019-07-01 22:03:57 +0200

    adding increment score functionality

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,12 +6,15 @@ class Counter extends React.Component {
         score: 0 
     };
 
+    incrementScore() {
+    }
+
     render() {
         return (
             <div className="counter">
                 <button className="counter-action decrement"> - </button>
                 <span className="counter-score"> {this.state.score} </span>
-                <button className="counter-action increment"> + </button>
+                <button className="counter-action increment" onClick={this.incrementScore}> + </button>
             </div>
         );
     }
