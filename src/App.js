7256a27c14e16aef4af61ada6330be42758d50f0
Preeti <singhp1992@gmail.com>
2019-07-01 22:37:48 +0200

    removing score from players obj

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ import Header from './components/Header'
 import Player from './components/Player'
 
 class App extends React.Component {
+
+  state = {
+    players: [{
+        name: "Preeti",
+        id: 1
+      },
+      {
+        name: "Simon",
+        id: 2
+      },
+      {
+        name: "Bani",
+        id: 3
+      },
+      {
+        name: "Simi",
+        id: 4
+      }]
+  }
+
+
   render() {
     return (
       <div className="scoreboard" id="root">
