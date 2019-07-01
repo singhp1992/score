f23c97084f8d4567186e189fb59af12997fb4938
Preeti <singhp1992@gmail.com>
2019-07-01 22:18:25 +0200

    decrement score function added and working

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -12,15 +12,15 @@ class Counter extends React.Component {
         });
     }
 
-    // decrementScore() {
-    //     this.setState({
-    //         score: this.state.score - 1
-    //     });
-    // }
+    decrementScore = () => {
+        this.setState({
+            score: this.state.score - 1
+        });
+    }
     render() {
         return (
             <div className="counter">
-                <button className="counter-action decrement" onClick={() => this.decrementScore()}> - </button>
+                <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
                 <span className="counter-score"> {this.state.score} </span>
                 <button className="counter-action increment" onClick={this.incrementScore}> + </button>
             </div>
