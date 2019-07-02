9ff24060de9a289a9b7d9458de7254ab5f14fd15
Preeti <singhp1992@gmail.com>
2019-07-02 10:18:35 +0200

    removed span with player state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,6 @@ function Header(props) {
         <header>
             <Stats />
             <h1>{props.title}</h1>
-            <span className="stats">Players: { props.totalPlayers }</span>
         </header>
     );
 }
