6085d0643cf4e526800da7d25f27112112c8bca7
Preeti <singhp1992@gmail.com>
2019-07-02 10:24:18 +0200

    creating reduce function

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Stats = (props) => {
 
     const totalPlayers = props.players.length;
-    const totalPoints = props.players.reduce();
+    const totalPoints = props.players.reduce( (total, player) => {
+        return total + player.score;
+    }, 0);
 
     return(
         <table className="stats" >
