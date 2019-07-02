d9eb4f3890b039393c944f0627aee5f3ebd69c2f
Preeti <singhp1992@gmail.com>
2019-07-02 10:33:04 +0200

    added a form and input tag

--- src/components/AddPlayerForm.jsx
+++ src/components/AddPlayerForm.jsx
@@ -3,6 +3,12 @@ import React, {Component} from 'react';
 class AddPlayerForm extends Component {
     render() {
         return(
+            <form>
+                <input 
+                    type="text"
+                    placeholder="Enter a player's name"
+                />
+            </form>
 
         );
     }
