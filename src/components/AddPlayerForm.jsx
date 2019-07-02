2b7fd8489b015889dcfcb69c4df8fe8b0d4c8e40
Preeti <singhp1992@gmail.com>
2019-07-02 10:54:47 +0200

    adding value to set state to clear form

--- src/components/AddPlayerForm.jsx
+++ src/components/AddPlayerForm.jsx
@@ -13,6 +13,7 @@ class AddPlayerForm extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.addPlayer(this.state.value);
+        this.setState({ value: ''});
     }
 
     render() {
