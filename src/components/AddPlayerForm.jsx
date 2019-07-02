ff5bcac1f2fa8ccd19e5cccc945ad2b49c00d44b
Preeti <singhp1992@gmail.com>
2019-07-02 10:43:13 +0200

    addig value to input

--- src/components/AddPlayerForm.jsx
+++ src/components/AddPlayerForm.jsx
@@ -5,12 +5,13 @@ class AddPlayerForm extends Component {
     state = {
         value: ''
     };
-    
+
     render() {
         return(
             <form>
                 <input 
                     type="text"
+                    value={this.state.value}
                     placeholder="Enter a player's name"
                 />
 
