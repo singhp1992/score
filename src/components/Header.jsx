30973e83aa8c5e11fe6b3795c97710ff0bab67b6
Preeti <singhp1992@gmail.com>
2019-07-02 10:18:09 +0200

    added stats comp to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import Stats from './Stats'
 function Header(props) {
     return (
         <header>
+            <Stats />
             <h1>{props.title}</h1>
             <span className="stats">Players: { props.totalPlayers }</span>
         </header>
