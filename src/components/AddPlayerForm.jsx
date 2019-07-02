92a0e4a240f0ecc2705df1e030a1c06532518f77
Preeti <singhp1992@gmail.com>
2019-07-02 10:33:41 +0200

    adding submit button

--- src/components/AddPlayerForm.jsx
+++ src/components/AddPlayerForm.jsx
@@ -8,6 +8,11 @@ class AddPlayerForm extends Component {
                     type="text"
                     placeholder="Enter a player's name"
                 />
+
+                <input 
+                    type="submit"
+                    value="Add Player"
+                />
             </form>
 
         );
