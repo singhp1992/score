de1a3d7c950b0249e33e8cd29dab87978dc2baa6
Preeti <singhp1992@gmail.com>
2019-11-01 14:27:27 +0100

    making counter comp stateful

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
-const Counter = (props) => {
-
-    let index = props.index;
-
-    return (
-        <div className="counter">
-            <button className="counter-action decrement" onClick={() => props.changeScore(index, -1)}> - </button>
-            <span className="counter-score"> { props.score } </span>
-            <button className="counter-action increment" onClick={() => props.changeScore(index, 1)}> + </button>
-        </div>
-    );
+class Counter extends React.Component {
+    render() {
+        return (
+            <div className="counter">
+                <button className="counter-action decrement" onClick={() => props.changeScore(props.index, -1)}> - </button>
+                <span className="counter-score"> {props.score} </span>
+                <button className="counter-action increment" onClick={() => props.changeScore(props.index, 1)}> + </button>
+            </div>
+        );
+    }
 }
 
 export default Counter
