c9bbfcd08f36354b8ac2b290ac76fd2e0a135e45
Preeti <singhp1992@gmail.com>
2019-11-01 14:33:50 +0100

    initializing state

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 class Counter extends React.Component {
     constructor() {
-        
+        super()
+        this.state = {
+            score: 0
+        };
     }
 
     render() {
