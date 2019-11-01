7519a66a0a38b5c8866ab88918373801693e41f1
Preeti <singhp1992@gmail.com>
2019-11-01 15:47:20 +0100

    adding handlescorechange function to onclick event;

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -14,9 +14,9 @@ class Counter extends React.Component {
     render() {
         return (
             <div className="counter">
-                <button className="counter-action decrement" onClick={() => this.props.changeScore(this.props.index, -1)}> - </button>
+                <button className="counter-action decrement" onClick={() => this.handleScoreChange(this.props.index, -1)}> - </button>
                 <span className="counter-score"> {this.state.score} </span>
-                <button className="counter-action increment" onClick={() => this.props.changeScore(this.props.index, 1)}> + </button>
+                <button className="counter-action increment" onClick={() => this.handleScoreChange(this.props.index, 1)}> + </button>
             </div>
         );
     }
