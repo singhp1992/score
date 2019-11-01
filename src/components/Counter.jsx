42fe33270897cab62a49c5c99113753fe1b1e7fc
Preeti <singhp1992@gmail.com>
2019-11-01 14:29:57 +0100

    accessing props using this.

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,9 +4,9 @@ class Counter extends React.Component {
     render() {
         return (
             <div className="counter">
-                <button className="counter-action decrement" onClick={() => props.changeScore(props.index, -1)}> - </button>
-                <span className="counter-score"> {props.score} </span>
-                <button className="counter-action increment" onClick={() => props.changeScore(props.index, 1)}> + </button>
+                <button className="counter-action decrement" onClick={() => this.props.changeScore(this.props.index, -1)}> - </button>
+                <span className="counter-score"> {this.props.score} </span>
+                <button className="counter-action increment" onClick={() => this.props.changeScore(this.props.index, 1)}> + </button>
             </div>
         );
     }
