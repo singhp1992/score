80d807bf88aa2b92f4cad922a07d66d4828c009a
Preeti <singhp1992@gmail.com>
2019-11-14 14:29:35 +0100

    cleaning up header comp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Stats from './Stats';
 import Stopwatch from './Stopwatch'
 
-function Header(props) {
+const Header = (props) => {
     return (
         <header>
             <h1>{props.title}</h1>
